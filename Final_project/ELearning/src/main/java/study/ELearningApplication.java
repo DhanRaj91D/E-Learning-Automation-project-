d2ed6c46ad15543a3d132e6f79bdea4add3670b4
dhumal dhanraj <dhanraj.sdhumal@gmail.com>
2022-04-03 01:26:51 +0530

    Final working Project

---
+++ Final_project/ELearning/src/main/java/study/ELearningApplication.java
@@ -0,0 +1,27 @@
+package study;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class ELearningApplication implements CommandLineRunner {
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELearningApplication.class, args);
+		
+		
+	
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+	}
+
+}
